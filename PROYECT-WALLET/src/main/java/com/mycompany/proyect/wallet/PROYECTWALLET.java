081feb1e038a5903baa19a317a854396a107de77
leidyva <vacacelaleidy499@gmail.com>
2024-06-21 10:12:07 -0500

    first commit

---
+++ PROYECT-WALLET/src/main/java/com/mycompany/proyect/wallet/PROYECTWALLET.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyect.wallet;
+
+/**
+ *
+ * @author fing.labcom
+ */
+public class PROYECTWALLET {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ PROYECT-WALLET/src/main/java/com/mycompany/proyect/wallet/Person.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyect.wallet;
+
+/**
+ *
+ * @author fing.labcom
+ */
+public class Person {
+    String name;
+    int age;
+    public Person(){
+        
+    }
+    public  Person(String name){
+        this.name=name;
+    }
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+}
